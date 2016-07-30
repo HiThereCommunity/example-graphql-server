cbbc608f92e675899a47d33ae4e73fb64cde046a
Dirk-Jan Rutten <dirkjanrutten@gmail.com>
2016-07-30 13:15:52 +0200

    Added Travis CI and added flow implementation to tutorial (#7)
    * Added flow implementation to the tutorial
    * Added Travis CI integration
    * Added Travis CI support
    * Removed the ‘|| exit 0’ from the test and check script so that it correctly works with Travis CI
    * Fixed typo in mochaSetup in package.json
    * Added babel-polyfill to make project work with node.js 4
    * Updated license tag in package.json
    * Update copyright notice at the top of files
    * Update the author field
    * Added the Travis CI Build status to the README

--- src/graphQL/__tests__/test.js
+++ src/graphQL/__tests__/test.js
@@ -1,5 +1,10 @@
 /**
- * Created by Dirk-Jan Rutten on 18/06/16.
+ * Copyright (c) 2016, Dirk-Jan Rutten
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
  */
 
 import {
